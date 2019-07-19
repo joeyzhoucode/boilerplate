1e7e34a682fdbe5f66a584e6fb4aff97450e71e6
Yi Zhou <yzhou.code@gmail.com>
2019-07-18 22:58:59 -0400

    Updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Group.create!(name: 'Global', viewers: [])
+Group.create!(name: 'Global', users: [])
