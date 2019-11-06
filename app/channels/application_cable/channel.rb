6b6175c80dfa3ab4201edd9b1a383b4c5c2e292f
joeyzhoucode <yzhou.code@gmail.com>
2019-11-06 18:29:10 -0500

    Fixing bugs

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,19 +1,4 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    identified_by :current_user
-
-    def connect
-      self.current_user = find_current_user
-    end
-
-    private
-
-    def find_current_user
-      if current_user = User.find_by(id: cookies.encrypted[:user_id])
-        current_user
-      else
-        reject_unauthorized_connection
-      end
-    end
   end
 end
