e9f189a2be1c7428a75c71b22ab9a8bede8568d6
joeyzhoucode <yzhou.code@gmail.com>
2019-11-06 18:07:47 -0500

    Making the client more lightweight

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,20 @@
-import React from "react";
-import { render } from "react-dom";
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { store, history} from './store';
 
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { routerMiddleware } from 'connected-react-router';
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 
-import { createBrowserHistory } from "history";
+import App from './components/App';
 
-import rootReducer from './reducers/rootReducer';
-import Root from "./containers/Root.jsx";
+ReactDOM.render((
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
 
-import "assets/scss/material-kit-react.scss?v=1.7.0";
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const hist = createBrowserHistory();
-const store = createStore(
-  rootReducer(hist),
-  composeEnhancer(applyMiddleware(thunk), applyMiddleware(routerMiddleware(hist)))
-);
-
-render(<Root store={store} history={hist} />, document.getElementById("root"));
-
+), document.getElementById('root'));
