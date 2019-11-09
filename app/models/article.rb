0dde536d93cadc25d097dd450bcd6e73e0dbfe69
joeyzhoucode <yzhou.code@gmail.com>
2019-11-09 17:34:24 -0500

    Fixing delete bug

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,8 +12,6 @@ class Article < ActiveRecord::Base
   validates :body, presence: true, allow_blank: false
   validates :slug, uniqueness: true, exclusion: { in: ['feed'] }
 
-  has_many :articles, dependent: :destroy
-
   before_validation do
     self.slug ||= "#{title.to_s.parameterize}-#{rand(36**6).to_s(36)}"
   end
