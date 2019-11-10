4d5df5d6d742bc070f00073ffcbc2ac559c01d7c
joeyzhoucode <yzhou.code@gmail.com>
2019-11-10 12:18:56 -0500

    Fixing pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,19 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all.includes(:user)
+    @articles_count = @articles.count
     @articles = @articles.order(created_at: :desc).offset(params[:offset] || 0).limit(params[:limit] || 20)
 
     @articles = @articles.tagged_with(params[:tag]) if params[:tag].present?
     @articles = @articles.authored_by(params[:author]) if params[:author].present?
     @articles = @articles.favorited_by(params[:favorited]) if params[:favorited].present?
 
-    @articles_count = @articles.count
-
     render json: { articles: @articles.as_json, articles_count: @articles_count }
   end
 
   def feed
     @articles = Article.includes(:user).where(user: User.find(session[:user_id]).following_users)
-
     @articles_count = @articles.count
-
     @articles = @articles.order(created_at: :desc).offset(params[:offset] || 0).limit(params[:limit] || 20)
 
     render json: { articles: @articles.as_json, articles_count: @articles_count }
@@ -64,7 +61,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    puts "++++++++++ #{params[:tag_list]} ++++++++++"
     params.require(:article).permit(:title, :body, :description, tag_list: [])
   end
 end
